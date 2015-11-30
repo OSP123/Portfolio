43d683a3d2652502fe23be8678c9baf7d4581dd1
OSP123 <omarpatel123@gmail.com>
2015-11-30 14:05:01 -0800

    uroutes

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ PortfolioBlog::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'main#index'
+  root 'static_pages#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
