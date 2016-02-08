3b064f0044552b36ddbce80829ff920e321c9043
OSP123 <omarpatel123@gmail.com>
2016-02-07 18:42:15 -0800

    fixed application

--- config/application.rb
+++ config/application.rb
@@ -21,17 +21,3 @@ module PortfolioBlog
     # config.i18n.default_locale = :de
   end
 end
-
-config.action_mailer.smtp_settings = {
-  address: "smtp.gmail.com",
-  port: 587,
-  domain: "<example.tld>",
-  user_name: "<username>",
-  password: "<password>",
-  authentication: :plain,
-  enable_starttls_auto: true
-}
-
-config.action_mailer.default_url_options = {
-  host: "yourdomain.tld"
-}
